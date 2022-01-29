02cb072dae90ae83f5a103f8df1a2b9103fd0a1f
didierganthier <didierganthierperan@gmail.com>
2022-01-29 17:31:21 -0500

    getting started chakra

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,21 @@ import { Container, Flex, VStack } from "@chakra-ui/react";
 const IndexPage = () => (
   <Container maxW="container.xl" p={0}>
     <Flex h="100vh" py={20} >
-      
+      <VStack 
+        w="full" 
+        h="full" 
+        p={10} 
+        spacing={10} 
+        alignItems="flex-start"
+      ></VStack>
+      <VStack 
+        w="full" 
+        h="full" 
+        p={10} 
+        spacing={10} 
+        alignItems="flex-start" 
+        bg="gray.50"
+      ></VStack>
     </Flex>
   </Container>
 );
